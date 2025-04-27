80a47f0056d3f81e99fc6b4c49065304b8ba3c22
Indrasis Datta <indrasis.datta@yahoo.co.in>
2025-04-27 12:11:30 +0530

    LSTM dependencies, nltk

--- test.py
+++ test.py
@@ -4,5 +4,9 @@ import numpy as np
 xpoints = np.array([0, 6])
 ypoints = np.array([0, 250])
 
-plt.plot(xpoints, ypoints)
-plt.show()
+# plt.plot(xpoints, ypoints)
+# plt.show()
+
+max_sequence_length = 4
+token_list = [12, 14, 25, 32, 45, 50]
+print(token_list[-(max_sequence_length-1):])
